9ac489310a39530be96684b80685d683579e7031
Ilya <ib258650@gmail.com>
2024-09-03 16:16:50 +0500

    change name model window and del button exit

--- RTTF/RTTF/urls.py
+++ RTTF/RTTF/urls.py
@@ -18,6 +18,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from accounts.views import *  # Импортируй функцию представления для главной страницы
+from puzzel.views import  *
 from django.conf import settings
 from django.conf.urls.static import static
 
